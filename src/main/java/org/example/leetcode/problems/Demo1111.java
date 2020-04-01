75fb22dc2cf85c8f234e674f129916a8a004bf2f
marcoo <papamilk@foxmail.com>
2020-04-01 20:38:01 +0800

    LeetCode 1111 位运算实现

--- src/main/java/org/example/leetcode/problems/Demo1111.java
+++ src/main/java/org/example/leetcode/problems/Demo1111.java
@@ -14,9 +14,21 @@ public class Demo1111 {
 //        String seq = "(()())";
         String seq = "()(())()";
         System.out.println(Arrays.toString(maxDepthAfterSplit(seq)));
+        System.out.println(Arrays.toString(maxDepthAfterSplitV2(seq)));
 
     }
 
+    /**
+     * 一种比较直接的方式，通过标记来实现，效率比位运算方式低。
+     *
+     * 这道题要让字符串的深度最低，保证连续出现的左括号要分到不同组即可。
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     *
+     * @param seq
+     * @return
+     */
     public static int[] maxDepthAfterSplit(String seq) {
         int[] arr = new int[seq.length()];
         boolean sign = true;
@@ -28,6 +40,28 @@ public class Demo1111 {
             } else {
                 sign = !sign;
                 arr[i] = sign ? 0 : 1;
+            }
+        }
+        return arr;
+    }
+
+    /**
+     * 使用位运算
+     * @param seq
+     * @return
+     */
+    public static int[] maxDepthAfterSplitV2(String seq) {
+        int[] arr = new int[seq.length()];
+        int depth = 0;
+        char[] chars = seq.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '(') {
+                depth++;
+                // depth & 1 等价于 depth % 2，位运算会比较快
+                arr[i] = depth & 1;
+            } else {
+                arr[i] = depth & 1;
+                depth--;
             }
         }
         return arr;
